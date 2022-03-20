9ab3a144d35c3f64defec19f4425e4067135d3ae
Steven Woodward <steveanthony999@gmail.com>
2022-03-20 01:50:39 -0700

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { css } from '@emotion/react';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 import CardComponent from './Components/CardComponent';
 import ButtonComponent from './Components/ButtonComponent';
@@ -7,18 +9,23 @@ import ButtonComponent from './Components/ButtonComponent';
 import './Styles/Styles.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [adviceData, setAdviceData] = useState();
 
   const generateQuote = () => {
+    setIsLoading(true);
     axios
       .get('https://api.adviceslip.com/advice')
-      .then((res) => setAdviceData(res.data))
+      .then((res) => {
+        setAdviceData(res.data);
+        setIsLoading(false);
+      })
       .catch((err) => console.log(err));
   };
 
   useEffect(() => {
     generateQuote();
-  }, []);
+  }, [setIsLoading, setAdviceData]);
 
   return (
     <div className='App'>
@@ -27,7 +34,13 @@ function App() {
           ADVICE #{adviceData && adviceData.slip.id}
         </h4>
         <h1 className='quote-text text-light-cyan'>
-          "{adviceData && adviceData.slip.advice}"
+          {isLoading ? (
+            <>
+              <ClipLoader color='#fff' size={40} />
+            </>
+          ) : (
+            <>"{adviceData && adviceData.slip.advice}"</>
+          )}
         </h1>
         <div className='rule'>
           <hr />
