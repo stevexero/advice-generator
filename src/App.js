e487c67f055a3808256453e06cd262b30033c5e0
Steven Woodward <steveanthony999@gmail.com>
2022-03-20 00:24:33 -0700

    set up components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import CardComponent from './Components/CardComponent';
+
+import './Styles/Styles.css';
+
 function App() {
+  const [adviceData, setAdviceData] = useState();
+
+  useEffect(() => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((res) => setAdviceData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='App'>
+      <CardComponent>
+        <h3>Advice #{adviceData && adviceData.slip.id}</h3>
+        <h1 className='text-light-cyan'>
+          {adviceData && adviceData.slip.advice}
+        </h1>
+        <div className='rule'>
+          <hr />
+          ''
+          <hr />
+        </div>
+      </CardComponent>
     </div>
   );
 }
---
+++ src/Components/CardComponent.jsx
@@ -0,0 +1,4 @@
+const CardComponent = ({ children }) => {
+  return <div className='Card'>{children}</div>;
+};
+export default CardComponent;
